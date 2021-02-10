e4a73a493e493d802548005eecbf3083a03eab06
Chibueze Ayogu <chibuezeayogu@hotmail.com>
2021-02-10 19:57:17 +0100

    Added set_post, show, update and destroy action and views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
-  def index; end
+  before_action :set_post, only: %i[edit show update destroy]
+
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
 
   def new
     @post = Post.new
@@ -9,10 +13,35 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  def show; end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
   end
 
 private
   def post_params
     params.require(:post).permit(:title, :content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
