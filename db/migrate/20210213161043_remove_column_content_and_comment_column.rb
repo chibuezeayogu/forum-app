6e67dfbc5e93a885f62948a787e58b88e0c1f95c
Chibueze Ayogu <chibuezeayogu@hotmail.com>
2021-02-13 19:01:33 +0100

    Remove migration file

--- db/migrate/20210213161043_remove_column_content_and_comment_column.rb
+++
@@ -1,6 +0,0 @@
-class RemoveColumnContentAndCommentColumn < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :posts, :content, :text
-    remove_column :comments, :comment, :text
-  end
-end
